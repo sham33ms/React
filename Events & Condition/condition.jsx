ca8020d05079f8d6ad7309b0c7dc3251105c02b1
shameem <shameem4contact@gmail.com>
2025-07-16 15:35:00 +0530

    React

--- Events & Condition/condition.jsx
+++ Events & Condition/condition.jsx
@@ -1,13 +1,18 @@
 import React, { useState } from 'react'
+export default condition
+import logo from './i2.jpg'
+import img from './i1.jpg'
+import './App.css'
 
 function condition() {
+  const [image, setimage] = useState(false)
     var [color,setcolor]=useState(false);
   return (
     <div>
       <button onClick={()=>setcolor(!color)}>Color change</button>
       <div style={{backgroundColor:color?'red':'blue'}}>Color</div>
+            <button onClick={()=>setimage(!image)}>Change the img</button>
+      <div >{image ?(<img src={logo} alt="alt1"  />):(<img src={img} alt="alt2" />)}</div>
     </div>
   )
 }
-
-export default condition
